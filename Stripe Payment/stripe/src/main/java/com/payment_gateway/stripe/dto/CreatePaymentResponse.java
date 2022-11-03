a3c9474c9f78d450afbafa100414f726f07880bb
MAELIOSA KYNE <19232624@studentmail.ul.ie>
2022-11-03 17:14:00 +0000

    created client.js, index.html + edited paymentController

--- Stripe Payment/stripe/src/main/java/com/payment_gateway/stripe/dto/CreatePaymentResponse.java
+++ Stripe Payment/stripe/src/main/java/com/payment_gateway/stripe/dto/CreatePaymentResponse.java
@@ -3,4 +3,12 @@ package com.payment_gateway.stripe.dto;
 public class CreatePaymentResponse {
     private String clientSecret;
     public CreatePaymentResponse(String clientSecret) { this.clientSecret = clientSecret;}
+    
+    public String getClientSecret() {
+      return clientSecret;
+    }
+
+    public void setClientSecret(String clientSecret) {
+      this.clientSecret = clientSecret;
+    }
   }
--- Stripe Payment/stripe/src/main/java/com/payment_gateway/stripe/web.controller/PaymentController.java
+++ Stripe Payment/stripe/src/main/java/com/payment_gateway/stripe/web.controller/PaymentController.java
@@ -3,32 +3,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.gson.annotations.SerializedName;
+import com.payment_gateway.stripe.dto.CreatePayment;
+import com.payment_gateway.stripe.dto.CreatePaymentResponse;
 import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentIntent;
 import com.stripe.param.PaymentIntentCreateParams;
+
 @RestController
 
 public class PaymentController {
-    static class CreatePayment {
-        @SerializedName("items")
-        Object[] items;
-    
-        public Object[] getItems() {
-          return items;
-        }
-      }
-    
-      static class CreatePaymentResponse {
-        private String clientSecret;
-        public CreatePaymentResponse(String clientSecret) {
-          this.clientSecret = clientSecret;
-        }
-      }
-    
     @PostMapping("/create-payment-intent")
     public CreatePaymentResponse createPaymentIntent(@RequestBody CreatePayment createPayment) throws StripeException {
-         PaymentIntentCreateParams params; PaymentIntentCreateParams createParams = new PaymentIntentCreateParams.Builder()
+         PaymentIntentCreateParams createParams = new PaymentIntentCreateParams.Builder()
                 .setAmount(15 * 100L)//create payment - payment calculator to be inserted here to calculate patients bill
                 .setCurrency("eur") 
                 .build();
