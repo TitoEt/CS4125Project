705c3f9a9260535d91cc7ad2d319bcb840e5f9e2
MAELIOSA KYNE <19232624@studentmail.ul.ie>
2022-11-09 17:46:07 +0000

    HTML and CSS added

---
+++ Stripe Payment/stripe/src/main/java/com/payment_gateway/stripe/web.controller/WebController.java
@@ -0,0 +1,14 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class WebController {
+ 
+
+@GetMapping("/")
+public String home(Model model){
+    return "index";
+}
+}
