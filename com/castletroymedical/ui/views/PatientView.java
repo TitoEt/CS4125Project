075ece498629913b2fc6e3cf78cb3952c99fb83f
Clodagh Walsh <clodaghwalsh17@gmail.com>
2022-11-16 15:09:54 +0000

    Add AppointmentReminder.java Add error handling to concrete subject Add methods to concrete observer to register and deregister
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

--- com/castletroymedical/ui/views/PatientView.java
+++ com/castletroymedical/ui/views/PatientView.java
@@ -3,17 +3,28 @@ package com.castletroymedical.ui.views;
 import com.castletroymedical.subscription.*;
 import com.castletroymedical.ui.notifications.AppointmentReminder;
 
-public class PatientView implements Observer{
+public class PatientView implements Observer {
     // TODO change to a list of subjects and have a set or a overloaded construcutor 
     private AppointmentReminder appointmentReminder;
 
+    public PatientView() {
+    }
+
     public PatientView(AppointmentReminder appointmentReminder) {
-        this.appointmentReminder = appointmentReminder;
-        appointmentReminder.attach(this);
+        appointmentReminderRegistration(appointmentReminder);
     }
 
     public void update() {
         System.out.println("Patient View");
         System.out.println("Appointment details " + appointmentReminder.getAppointment());
     } 
+
+    public void appointmentReminderRegistration(AppointmentReminder appointmentReminder) {
+        this.appointmentReminder = appointmentReminder;
+        appointmentReminder.attach(this);
+    }
+
+    public void appointmentReminderDeregistration() {
+        appointmentReminder.detach(this); 
+    }
 }
