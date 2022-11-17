06b5643ef260bd41a6f6711cc500481098900f37
TitoEt <72010582+TitoEt@users.noreply.github.com>
2022-11-17 11:44:44 +0000

    Revert "Billing Package"

--- com/castletroymedical/billing/ModifierState.java
+++
@@ -1,9 +0,0 @@
-package com.castletroymedical.billing;
-
-public abstract class ModifierState {
-    public abstract double calculateProcedureCost(double baseCost);
-    public abstract double calculateConsultationFee(double baseCost);
-    public abstract double calculateMonitoringFee(double baseCost, int days);
-    public abstract double calculateBedRate(double baseCost, int days);
-
-}
