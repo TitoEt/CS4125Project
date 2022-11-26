4c1ca3acf4fde8b32b86873b2edda19d5a9042f5
Replit user <>
2022-11-26 00:51:24 +0000

    Abstract builder remove

--- src/main/java/com/castletroymedical/billing/InvoiceBuilder.java
+++ src/main/java/com/castletroymedical/billing/InvoiceBuilder.java
@@ -10,7 +10,9 @@ import com.castletroymedical.billing.invoice.PrivateRoom;
 import com.castletroymedical.billing.invoice.Procedure;
 import com.castletroymedical.billing.invoice.WardBed;
 
-public class InvoiceBuilder extends Builder {
+public class InvoiceBuilder {
+
+    private Invoice invoice;
     
     public InvoiceBuilder(String type){ 
         this.invoice = invoiceFactory(type);
@@ -29,48 +31,39 @@ public class InvoiceBuilder extends Builder {
         }
         return out;
     }
-
-    @Override
+ 
     public void buildConsultation() {
         this.invoice = new Consultation(this.invoice);
     }
-
-    @Override
+ 
     public void buildMonitoring() {
         this.invoice = new Monitoring(this.invoice);
     }
-
-    @Override
+ 
     public void buildMonitoring(int days) {
         this.invoice = new Monitoring(days, this.invoice);
     }
-
-    @Override
+ 
     public void buildPrivateRoom() {
         this.invoice = new PrivateRoom(this.invoice);
     }
-
-    @Override
+ 
     public void buildPrivateRoom(int days) {
         this.invoice = new PrivateRoom(days, this.invoice);
     }
-
-    @Override
+ 
     public void buildProcedure(String name) {
         this.invoice = new Procedure(name, this.invoice);
     }
-
-    @Override
+ 
     public void buildWardBed() {
         this.invoice = new WardBed(this.invoice);
     }
-
-    @Override
+ 
     public void buildWardBed(int days) {
         this.invoice = new WardBed(days, this.invoice);;
     }
-
-    @Override
+ 
     public Invoice getInvoice() { 
         return this.invoice;
     }
