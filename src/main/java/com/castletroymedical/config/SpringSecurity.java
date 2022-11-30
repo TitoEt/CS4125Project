daf468032060ec1c9a44932693b51fbd98557855
Replit user <>
2022-11-30 12:29:25 +0000

    Edited security perms for access

--- src/main/java/com/castletroymedical/config/SpringSecurity.java
+++ src/main/java/com/castletroymedical/config/SpringSecurity.java
@@ -10,47 +10,48 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.SecurityFilterChain; 
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurity {
 
-    // @Autowired
-    // private UserDetailsService userDetailsService;
+    @Autowired
+    private UserDetailsService userDetailsService; 
 
     @Bean
     public static PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
-    // @Bean
-    // public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    //     http.csrf().disable()
-    //             .authorizeHttpRequests()
-    //             .requestMatchers("/register/**").permitAll()
-    //             .requestMatchers("/index").permitAll()
-    //             .requestMatchers("/users").hasRole("ADMIN")
-    //             .and().
-    //             formLogin(
-    //                     form -> form
-    //                             .loginPage("/login")
-    //                             .loginProcessingUrl("/login")
-    //                             .defaultSuccessUrl("/users")
-    //                             .permitAll()
-    //             ).logout(
-    //                     logout -> logout
-    //                             .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-    //                             .permitAll()
-    //             );
-    //     return http.build();
-    // }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/register/**").permitAll()
+                .requestMatchers("/index").permitAll()
+                .requestMatchers("/stripe/**").permitAll()
+                .requestMatchers("/hospital-procedure/**").hasRole("ADMIN")
+                .requestMatchers("/users").hasRole("ADMIN");
+                // .and().
+                // formLogin(
+                //         form -> form
+                //                 .loginPage("/login")
+                //                 .loginProcessingUrl("/login")
+                //                 .defaultSuccessUrl("/users")
+                //                 .permitAll()
+                // ).logout(
+                //         logout -> logout
+                //                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                //                 .permitAll()
+                // );
+        return http.build();
+    }
 
-    // @Autowired
-    // public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-    //     auth
-    //             .userDetailsService(userDetailsService)
-    //             .passwordEncoder(passwordEncoder());
-    // }
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
 }
