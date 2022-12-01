b3656ea33778f01c853a395be851d21f9ddb2000
Clodagh Walsh <clodaghwalsh17@gmail.com>
2022-12-01 15:59:40 +0000

    Add Invoice test suite
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

--- src/test/java/com/castletroymedical/TestInvoice.java
+++ src/test/java/com/castletroymedical/TestInvoice.java
@@ -2,15 +2,31 @@ package com.castletroymedical;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
 
 import com.castletroymedical.billing.invoice.*;
 
 public class TestInvoice {
-    double expected = 100;
 
-    @Test
-    public void invoiceTest() {
-       assertEquals(expected, new Procedure("Xray", 150, new PrivateInvoice()).getCharge(), 0.0001);
+    @ParameterizedTest
+    @MethodSource("generateInvoices")
+    public void testInvoice(Invoice invoice, double expected) {
+        assertEquals(expected, invoice.getCharge());
     }
+
+    private static Stream<Arguments> generateInvoices() {
+        return Stream.of(
+            Arguments.of(new Procedure("MRI", 150, new PrivateInvoice()), 150),
+            Arguments.of(new Procedure("Vaccinations", 40, new MedicalCardInvoice()), 20),
+            Arguments.of(new Procedure("CAT Scan", 75, new InsuredInvoice()), 90),
+            Arguments.of(new Procedure("Thyroid procedure", 650, new Consultation(new PrivateInvoice())), 680),
+            Arguments.of(new Procedure("Cryotherapy", 60, new Consultation(new MedicalCardInvoice())), 30),
+            Arguments.of(new Procedure("Brain Stem Augmentation", 8000, new Consultation(new PrivateRoom(5, new Monitoring(3, new InsuredInvoice())))), 10695),
+            Arguments.of(new Procedure("Chemotherapy", 150, new WardBed(2, new Monitoring(2, new MedicalCardInvoice()))), 220)
+        );
+    }
+
 }
