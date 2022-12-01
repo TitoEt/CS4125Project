124420f25455082ad2ac8613008c1ac9b47e23d6
Clodagh Walsh <clodaghwalsh17@gmail.com>
2022-12-01 23:34:55 +0000

    Whole repo clean up
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

--- src/main/java/com/castletroymedical/config/LoadDatabase.java
+++ src/main/java/com/castletroymedical/config/LoadDatabase.java
@@ -1,6 +1,6 @@
-// package com.castletroymedical.config;
+package com.castletroymedical.config;
 
-import ava.util.Date;
+import java.util.Date;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -9,10 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import com.castletroymedical.dto.PatientDto;
 import com.castletroymedical.dto.UserDto;
 import com.castletroymedical.entity.HospitalProcedureEntity;
-import 
-
-import com.castletroymedical.repository.PatientRepository;
-import com.castletroymedical.service.PatientService;
+import com.castletroymedical.repository.HospitalProcedureRepository;
 import com.castletroymedical.service.impl.PatientServiceImpl;
 import com.castletroymedical.service.impl.UserServiceImpl;
 
@@ -24,12 +21,13 @@ public class LoadDatabase {
         return args -> {
             procedureRepository.save(new HospitalProcedureEntity(1L, "Brain Stem Augmentation",8000.0));
             procedureRepository.save(new HospitalProcedureEntity(2L,"Spinal Realignment",2300.0));
-            procedureRepository.save(new HospitalProcedure           procedureRepository.save(new HospitalProcedureEntity(4L,"MRI",125.0));
+            procedureRepository.save(new HospitalProcedureEntity(3L,"Blood Test",45.0));
+            procedureRepository.save(new HospitalProcedureEntity(4L,"MRI",125.0));
             procedureRepository.save(new HospitalProcedureEntity(5L,"Cornea Replacement",786.0));
             procedureRepository.save(new HospitalProcedureEntity(6L,"CAT Scan",75.0));
             procedureRepository.save(new HospitalProcedureEntity(7L,"Joint Injection",65.0));
             procedureRepository.save(new HospitalProcedureEntity(8L,"Cryotherapy",60.0));
-           procedureRepository.save(new HospitalProcedureEntity(9L,"Cervical Smear",90.0));
+            procedureRepository.save(new HospitalProcedureEntity(9L,"Cervical Smear",90.0));
             procedureRepository.save(new HospitalProcedureEntity(10L,"Chemotherapy",557.0));
             procedureRepository.save(new HospitalProcedureEntity(11L,"Kidney Function Assessment",811.0));
             procedureRepository.save(new HospitalProcedureEntity(12L,"Vaccinations",40.0));
